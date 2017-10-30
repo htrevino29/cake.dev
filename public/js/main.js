8fd9aecdde68e4522a28f45522f2cacb34ca7efb
htrevino29 <htrevino29@gmail.com>
2017-10-30 00:51:24 -0500

    starting new personal page index updated.

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+document.getElementById("para1").innerHTML = formatAMPM();
+
+function formatAMPM() {
+var d = new Date(),
+    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+    ampm = d.getHours() >= 12 ? 'pm' : 'am',
+    months = ['1','2','3','4','5','6','7','8','9','10','11','12'],
+    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+return months[d.getMonth()]+'/'+d.getDate()+'/'+d.getFullYear()+' '+hours+':'+minutes+ampm+' CST';
+alert('fuck');
+};
+
